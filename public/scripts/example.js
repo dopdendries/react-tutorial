a629b5022b4c5f1212fe6427c11ab2615b956ceb
Daniel Opden Dries <dopdendries@Daniels-iMac.local>
2016-04-17 17:54:50 -0700

    Class Creation / use part of tutorial

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,4 +1,42 @@
 /**
- *  Tutorial
+ *  Working Tutorial File
  */
 
+///[ COMMENT BOX ]
+var CommentBox = React.createClass({
+  render: function(){
+    return (
+      <div className="commentBox">
+        <h2>Comments</h2>
+        <CommentList />
+        <CommentForm />
+      </div>
+      );
+  }
+});
+
+//[ COMMENT LIST ]
+var CommentList = React.createClass({
+  render: function(){
+    return (
+      <div className="commentList">
+        Hi, I am a CommentList.
+      </div>
+    );
+  }
+});
+
+//[ COMMENT FORM ]
+var CommentForm = React.createClass({
+  render: function(){
+    return(
+      <div className="commentForm">
+        Hello, I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+
+//Render a CommentBox in the content div
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
